817da1d67d179d0e4f7d1d011e7b27378ab3caff
fparra88 <fernandoparrav@outlook.com>
2026-03-18 23:48:28 -0600

    actualizacion backend v1

--- routers/compras.py
+++ routers/compras.py
@@ -14,6 +14,10 @@ def get_db_connection():
         database=os.getenv("DB_NAME")
     )
 
+class compraPromedio(BaseModel):   
+    sku: str     
+    costo_prom: float
+
 # Modelo para recibir datos de compra
 class CompraModel(BaseModel):
     sku: str  # Identificador del producto
@@ -71,6 +75,72 @@ async def recibir_compra(compra: CompraModel):
             return {
                 "mensaje": "El producto no existe revisa el SKU"
             }
+
+    except mysql.connector.Error as err:
+        # Si la base de datos truena, me entero qué pasó
+        print(f"Error en BD: {err}")
+        raise HTTPException(status_code=500, detail="Error al procesar la compra en BD")
+
+    finally:
+        # Siempre cierro la puerta al salir
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
+
+# ---- Promedio de compras ----
+@router.get("/ultimos-costos/{sku}")
+async def obtener_ultimos_costos(sku: str):
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    
+    try:
+        # Buscamos los últimos 2 costos de ese SKU ordenados por la fecha más reciente
+        query = """
+            SELECT costo_total 
+            FROM compras 
+            WHERE sku = %s 
+            ORDER BY fecha_registro DESC 
+            LIMIT 2
+        """
+        cursor.execute(query, (sku,))
+        resultados = cursor.fetchall()
+        
+        # Extraemos los costos y los guardamos en una lista de Python
+        # Si no hay compras, devolverá una lista vacía []
+        costos_historicos = [float(fila["costo_total"]) for fila in resultados if fila["costo_total"] is not None]
+        
+        return {"sku": sku, "costos": costos_historicos}
+        
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=str(e))
+    finally:
+        cursor.close()
+        conn.close()
+
+@router.post("/costoPromedio") # Endpoint para registrar costo promedio
+async def costo_promedio(cprom: compraPromedio):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    
+    try:
+        cursor.execute(
+                "UPDATE productos SET costo_total = %s WHERE sku = %s",
+                (cprom.costo_prom, cprom.sku)
+            )
+        
+        conn.commit()
+        
+        # Respuesta
+        if cursor.rowcount > 0:
+            return {
+                "msg": "Compra actualizada",
+                "sku": cprom.sku,
+                "costo_promedio": cprom.costo_prom
+            }
+        else:
+            # retornamos si no encontramos SKU
+            return {"msg": "No se encontró el SKU o no hubo cambios", "sku": cprom.sku}
 
     except mysql.connector.Error as err:
         # Si la base de datos truena, me entero qué pasó
